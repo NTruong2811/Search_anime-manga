52bdf4a14521d2054dd39f3903a9733636f9b84f
truongnn15584 <truongnnph15584@fpt.edu.vn>
2022-07-07 10:39:07 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="title">Tìm kiếm Anime</div>
+  <div class="search">
+    <form action="">
+      <input
+        type="search"
+        class="search-box"
+        v-model="search_query"
+        @change="HandleSearch"
+      />
+      <i class="fa-solid fa-magnifying-glass search-icon"></i>
+    </form>
+  </div>
+
+  <div class="list_result">
+    <card  v-for="anime in search_result" :anime="anime" />
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import { ref } from "vue";
+// import axios from "axios";
+import card from "./components/card.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const search_query = ref("");
+    const search_result = ref([]);
+    const api_anime = `https://api.jikan.moe/v3/search/anime?q=`;
+    const HandleSearch = async () => {
+     search_result.value = await fetch(api_anime + search_query.value)
+				.then(res => res.json())
+				.then(data => data.results);
+
+        console.log(search_result.value);
+    };
+    return {
+      search_query,
+      api_anime,
+      search_result,
+      HandleSearch,
+    };
+  },
+  methods: {},
+  components: { card },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="css">
+body {
+  background-color: #2a2e37;
+  font-family: sans-serif;
+}
+
+.title {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #00fede;
+  font-size: 40px;
+  margin: 100px 0px 50px 0px;
+}
+.search {
+  width: 400px;
+  margin: auto;
+  position: relative;
+}
+.search-box {
+  width: 400px;
+  padding: 20px;
+  font-size: 25px;
+  background-color: #242628;
+  border: none;
+  outline: none;
+  color: white;
+}
+.search-icon {
+  color: #00fede;
+  font-size: 30px;
+  position: absolute;
+  right: 5%;
+  top: 30%;
+}
+
+.list_result {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 70px;
+}
+
+[type="search"]::-webkit-search-cancel-button,
+[type="search"]::-webkit-search-decoration {
+  -webkit-appearance: none;
+  appearance: none;
 }
 </style>
---
+++ src/components/card.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+    <a :href="anime.url">
+      <div class="img">
+        <img
+          :src="anime.image_url"
+          alt=""
+        />
+      </div>
+      <div class="name">
+        <h1>{{anime.title}}</h1>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+    props:['anime']
+}
+</script>
+
+<style>
+a {
+  color: white;
+  text-decoration: none;
+}
+.card {
+  color: white;
+  width: 225px;
+  height: 317px;
+  position: relative;
+  margin: 20px 20px;
+}
+.card .name{
+    position: absolute;
+    bottom: 0px;
+    background-color: rgba(0, 0, 0, 0.57);
+    width: 225px;
+    font-size: 10px;
+    height: 60px;
+    text-align: center;
+}
+</style>
